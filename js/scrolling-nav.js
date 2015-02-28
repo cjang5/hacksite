c8898e5ce7d6b6b052596df655f23f26b87cff9f
Christopher Jang <jangerino@wirelessprvnat-172-17-186-51.near.illinois.edu>
2015-02-28 16:11:42 -0600

    Added transition for How it Works

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
     $('.main_image').fadeIn(1000).addClass('appear');
 });
 
-/*
-$(window).scroll(function() {
-  if ($(".navbar").offset().top == 0) {
-    $(".main_image").fadeOut();
-  } else if ($(".navbar").offset().top > 45) {
-    $(".main_image").fadeIn();
-    $(".main_image").addClass("appear");
-  }
-}); */
-
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 100) {
@@ -19,7 +9,18 @@ $(window).scroll(function() {
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+    
+    if ($(".navbar").offset().top > 500) {
+        $(".about_text").fadeIn(1000).addClass("appear");
+    }
 });
+
+/*
+$(window).scroll(function() {
+    if ($".navbar").offset().top > 300) {
+        $(".about_text").fadeIn(1000).addClass("appear");
+    }
+});*/
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
