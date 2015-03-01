d965f6f8f78ed6ce2098386deb4a35a2c45d85ec
Christopher Jang <jangerino@wirelessprvnat-172-17-186-51.near.illinois.edu>
2015-02-28 21:33:50 -0600

    Gif works, now for translating

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $('.main_image').fadeIn(1000).addClass('appear');
+    $('.main_gif').fadeIn(1000).addClass('appear');
 });
 
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
+    if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
